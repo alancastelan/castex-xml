bea3ebe15b1a342e90063f5a498395be926bc3d7
CastechCore <100005842+CastechCore@users.noreply.github.com>
2023-02-09 09:03:28 -0300

    last commit

--- test.mjs
+++ test.mjs
@@ -2,14 +2,14 @@
  * @ Author: Alan Castelan - Castech
  * @ Create Time: 2022-12-29 11:50:24
  * @ Modified by: Alan Castelan
- * @ Modified time: 2023-02-09 08:55:00
+ * @ Modified time: 2023-02-09 09:03:12
  * @ Description: email: alancastelan.ac@gmail.com
  * discord - https://discord.gg/vXXhu5h6
  */
 
 
 
-import CastexXml from "../index.js";
+import CastexXml from "index.js";
 
 /* Exemplo de chamada do Certificado */
 var Certificado = {
